16da1ad48743faba5519095b3424053e5e816763
GharbinBern <bg2696@nyu.edu>
2026-02-02 13:27:35 +0400

    edit to use new page names

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,9 +1,15 @@
 import { lazy } from 'react'
 
-const HomePage = lazy(() => import('./features/HomePage'))
-const DriverList = lazy(() => import('./features/DriverList'))
+const Home = lazy(() => import('./pages/Home'))
+const Driver = lazy(() => import('./pages/Driver'))
+const Races = lazy(() => import('./pages/Races'))
+const Teams = lazy(() => import('./pages/Teams'))
+const Laps = lazy(() => import('./pages/Laps'))
 
 export const routes = [
-  { path: '/', element: <HomePage /> },
-  { path: '/drivers', element: <DriverList /> },
+  { path: '/', element: <Home /> },
+  { path: '/races', element: <Races /> },
+  { path: '/drivers', element: <Driver /> },
+  { path: '/teams', element: <Teams /> },
+  { path: '/laps', element: <Laps /> },
 ]
